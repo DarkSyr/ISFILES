0c396dc5be3b4a9af64fe569fe1ff09301e6ebb5
DarkSyr <liam.krall29@d155.org>
2026-08-18 18:41:55 -0500

    Update UV configuration paths to relative URLs

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: "/p/8GZR8E42/service/",
-    bare: "/p/8GZR8E42/bare/",
+    prefix: "./service/",
+    bare: "./bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/p/8GZR8E42/uv.handler.js",
-    bundle: "/p/8GZR8E42/uv.bundle.js",
-    config: "/p/8GZR8E42/uv.config.js",
-    sw: "/p/8GZR8E42/uv.sw.js"
+    handler: "./uv.handler.js",
+    bundle: "./uv.bundle.js",
+    config: "./uv.config.js",
+    sw: "./uv.sw.js"
 };
